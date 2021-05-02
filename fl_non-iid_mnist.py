83c0304b44133ff1cda67b911dc0975fef83fc6b
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2021-05-02 15:03:41 +0530

    Add files via upload

---
+++ fl_non-iid_mnist.py
@@ -0,0 +1,123 @@
+import numpy as np
+import random
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import backend as K
+
+from fl_mnist_implementation_tutorial_utils import *
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize']=10,8
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+#Sort the data for Non-IID partition
+idx = np.argsort(y_train)
+x_train = x_train[idx]
+y_train = y_train[idx]
+
+# train set / data 
+x_train = x_train.reshape(-1, 28*28)
+x_train = x_train.astype('float32') / 255
+x_test = x_test.reshape(-1, 28*28)
+x_test = x_test.astype('float32') / 255
+
+# train set / target 
+y_train = tf.keras.utils.to_categorical(y_train , num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test , num_classes=10)
+
+#create clients
+clients = create_clients(x_train, y_train, num_clients=100, initial='client')
+
+#process and batch the training data for each client
+clients_batched = dict()
+for (client_name, data) in clients.items():
+    clients_batched[client_name] = batch_data(data)
+    
+#process and batch the test set  
+test_batched = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(len(y_test))
+
+comms_round = 300
+
+#create optimizer
+lr = 0.01
+#lr = 0.1 
+loss='categorical_crossentropy'
+metrics = ['accuracy']
+optimizer = SGD(lr=lr, 
+                decay=lr / comms_round, 
+                momentum=0.9
+               )
+
+#initialize global model
+smlp_global = SimpleMLP()
+global_model = smlp_global.build(784, 10)
+
+store_global_acc = []
+ 
+#commence global training loop
+for comm_round in range(comms_round):
+            
+    # get the global model's weights - will serve as the initial weights for all local models
+    global_weights = global_model.get_weights()
+    
+    #initial list to collect local model weights after scalling
+    scaled_local_weight_list = list()
+
+    #randomize client data - using keys
+    client_names= list(clients_batched.keys())
+    client_names = random.choices(client_names, k=10)
+    
+    #loop through each client and create new local model
+    for client in client_names:
+        smlp_local = SimpleMLP()
+        local_model = smlp_local.build(784, 10)
+        local_model.compile(loss=loss, 
+                      optimizer=optimizer, 
+                      metrics=metrics)
+        
+        #set local model weight to the weight of the global model
+        local_model.set_weights(global_weights)
+        
+        #fit local model with client's data
+        local_model.fit(clients_batched[client], epochs=1, verbose=0)
+        
+        #scale the model weights and add to list
+        #scaling_factor = weight_scalling_factor(clients_batched, client)
+        scaling_factor = 0.1
+        scaled_weights = scale_model_weights(local_model.get_weights(), scaling_factor)
+        scaled_local_weight_list.append(scaled_weights)
+        
+        #clear session to free memory after each communication round
+        K.clear_session()
+        
+    #to get the average over all the local model, we simply take the sum of the scaled weights
+    average_weights = sum_scaled_weights(scaled_local_weight_list)
+    
+    #update global model 
+    global_model.set_weights(average_weights)
+
+    #test global model and print out metrics after each communications round
+    for(X_test, Y_test) in test_batched:
+        global_acc, global_loss = test_model(X_test, Y_test, global_model, comm_round)
+        store_global_acc.append(100*global_acc)
+
+plt.plot(store_global_acc)
+plt.xlabel("Communication round")
+plt.ylabel("Accuracy (%)")
+plt.show()
